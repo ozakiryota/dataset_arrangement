36dd01022fafba222f938c1518329a65e555a7a9
ozakiryota <kb22056@gmail.com>
2023-01-04 02:09:59 +0900

    Rename files

--- shell/pixel_count_divider.sh
+++ shell/pixel_count_divider.sh
@@ -2,8 +2,8 @@
 
 exec_pwd=$(cd $(dirname $0); pwd)
 
-python3 $exec_pwd/../pyscr/file_list_divider.py \
-    --read_csv_path $HOME/dataset/airsim/tmp/file_list.csv \
+python3 $exec_pwd/../pyscr/pixel_count_divider.py \
+    --read_csv_path $HOME/dataset/airsim/sample/file_list.csv \
     --query_col 2 \
     --query_pixel_value 184 145 182 \
     --min_num_query_pixels 5000
