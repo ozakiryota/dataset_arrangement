282c1d0f5e269441a9ba2718f0f56e2c5d93a02c
ozakiryota <kb22056@gmail.com>
2023-01-07 17:19:48 +0900

    Fix --dir_list to --read_dir_path for file_list_write.py

--- pyscr/file_list_write.py
+++ pyscr/file_list_write.py
@@ -10,25 +10,25 @@ class FileListWrite:
     
     def setArgument(self):
         arg_parser = argparse.ArgumentParser()
-        arg_parser.add_argument('--dir_list', nargs='+', type=str, required=True)
-        arg_parser.add_argument('--write_csv_path', default='file_list.csv')
+        arg_parser.add_argument('--read_dir_path', required=True)
+        arg_parser.add_argument('--write_csv_name', default='file_list.csv')
         arg_parser.add_argument('--target_extension', nargs='+', type=str, default=['png', 'jpg', 'jpeg', 'PNG', 'JPG', 'JPEG'])
         return arg_parser
 
     def exec(self):
-        file_list_list = []
-        for dir in self.args.dir_list:
-            file_list = glob.glob(dir + '/*')
-            extracted_file_list = []
-            for file_path in file_list:
-                extension = file_path.split('.')[-1]
-                if extension in self.args.target_extension:
-                    extracted_file_list.append(file_path)
-            file_list_list.append(extracted_file_list)
-        file_list_list = list(zip(*file_list_list))
-        with open(self.args.write_csv_path, 'w') as file_list_csv:
+        file_path_list = glob.glob(self.args.read_dir_path + '/*')
+        extracted_file_list = []
+        for file_path in file_path_list:
+            extension = file_path.split('.')[-1]
+            if extension in self.args.target_extension:
+                file_name = os.path.basename(file_path).split('.')[0]
+                extracted_file_list.append(file_name)
+        extracted_file_list = list(zip(*[extracted_file_list]))
+        write_csv_path = os.path.join(self.args.read_dir_path, self.args.write_csv_name)
+        with open(write_csv_path, 'w') as file_list_csv:
             csv_writer = csv.writer(file_list_csv)
-            csv_writer.writerows(file_list_list)
+            csv_writer.writerows(extracted_file_list)
+        print("Save:", write_csv_path)
 
 
 if __name__ == '__main__':
--- shell/file_list_write.sh
+++ shell/file_list_write.sh
@@ -3,5 +3,5 @@
 exec_pwd=$(cd $(dirname $0); pwd)
 
 python3 $exec_pwd/../pyscr/file_list_write.py \
-    --dir_list $HOME/dataset/airsim/sample_data/img $HOME/dataset/airsim/sample_data/label \
-    --write_csv_path $HOME/dataset/airsim/sample_data/written_file_list.csv
+    --read_dir_path $HOME/dataset/airsim/sample_data \
+    --write_csv_name new_file_list.csv
