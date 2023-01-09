d7b122bf41bace3e6daf5929bad7aec4553ccb04
ozakiryota <kb22056@gmail.com>
2023-01-09 11:07:53 +0900

    Add args for file_list_write.sh

--- shell/file_list_write.sh
+++ shell/file_list_write.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo "Error: Usage: ./file_list_write.sh DIR_PATH"
+    exit 1
+fi
+
+path_first_char=`echo $1 | cut -c 1`
+if [ $path_first_char == '/' ] || [ $path_first_char == '~' ]; then
+    dir_path=$1
+else
+    dir_path="$(pwd)/$1"
+fi
+
 exec_pwd=$(cd $(dirname $0); pwd)
 
 python3 $exec_pwd/../pyscr/file_list_write.py \
-    --read_dir_path $HOME/dataset/airsim/sample_data \
-    --write_csv_name new_file_list.csv
+    --read_dir_path $dir_path \
+    --write_csv_name file_list.csv
