1b14d6b5e9528ab90e368f5a4b3f59b950126829
ozakiryota <kb22056@gmail.com>
2023-01-09 09:54:25 +0900

    Refactor rgb_to_gray.py

--- pyscr/rgb_to_gray.py
+++ pyscr/rgb_to_gray.py
@@ -2,7 +2,6 @@ import argparse
 import os
 import csv
 import cv2
-from PIL import Image
 import numpy as np
 
 class RgbToGray:
@@ -13,18 +12,13 @@ class RgbToGray:
         arg_parser = argparse.ArgumentParser()
         arg_parser.add_argument('--read_csv_path', required=True)
         arg_parser.add_argument('--target_col', type=int, default=0)
-        arg_parser.add_argument('--flag_gamma', action='store_true')
+        arg_parser.add_argument('--flag_gamma_correction', action='store_true')
         arg_parser.add_argument('--write_dir_path')
         return arg_parser
 
-    def grayscle(self, img_path):
-        img = Image.open(img_path).convert('L')
-        img = np.array(img, dtype=np.uint8)
-        return img
-
     def gammaGrayscle(self, img_path):
         img = cv2.imread(img_path)
-        gamma22LUT = numpy.array([pow(x/255.0 , 2.2) for x in range(256)], dtype='float32')
+        gamma22LUT = np.array([pow(x/255.0 , 2.2) for x in range(256)], dtype='float32')
         img = cv2.LUT(img, gamma22LUT)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img = pow(img, 1.0/2.2) * 255
@@ -41,10 +35,10 @@ class RgbToGray:
             for file_list in csv_reader:
                 img_name = file_list[self.args.target_col]
                 img_path = os.path.join(root_dir_path, img_name)
-                if self.args.flag_gamma:
+                if self.args.flag_gamma_correction:
                     img = self.gammaGrayscle(img_path)
                 else:
-                    img = self.grayscle(img_path)
+                    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
                 write_path = os.path.join(self.args.write_dir_path, img_name.split('.')[0] + "_grayscaled.png")
                 cv2.imwrite(write_path, img)
                 print("Save:", write_path)
--- shell/rgb_to_gray.sh
+++ shell/rgb_to_gray.sh
@@ -3,6 +3,7 @@
 exec_pwd=$(cd $(dirname $0); pwd)
 
 python3 $exec_pwd/../pyscr/rgb_to_gray.py \
-    --read_csv_path $HOME/dataset/airsim/center_road_south2north_1000_blured/file_list.csv \
-    --target_col 0 \
-    --write_dir_path $HOME/dataset/airsim/center_road_south2north_1000_blured_graysclaled
+    --read_csv_path $HOME/dataset/airsim/sample_data/file_list.csv \
+    --target_col 1 \
+    --flag_gamma_correction \
+    --write_dir_path $HOME/dataset/airsim/sample_data_grayscaled
